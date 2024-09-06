3482ffc94af1444dc7958ff423fa66c787177506
= <=>
2024-09-06 19:24:47 +0100

    assignment

--- e-commercer/js/main.js
+++ e-commercer/js/main.js
@@ -1,32 +1,28 @@
-// const apiKey = '53d3c02e056edba8db0c02bf0dea15ed';
+
+// fetch('https://dummyjson.com/products')
+// .then(res => res.json())
+// .then(console.log);
 
 
+fetch('https://dummyjson.com/products')
+    .then(response => response.json())
+    .then(data => {
+        const productsContainer = document.getElementById('products-container');
 
+        data.products.forEach(product => {
+            const productHTML = `
+                <div class="product">
+                    <img src="${product.thumbnail}" alt="${product.title}">
+                    <h2>${product.title}</h2>
+                    <h3>$${product.price}</h3>${product.discountPercentage}% off
+                    <p>Availability Status: ${product.availabilityStatus}</p>
+                    <button class="add-to-cart-btn">Add to Cart</button>
+                </div>
+            `;
 
-fetch('https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/address?postalcode=82009&page=1&pagesize=100', {
-  method: 'GET',
-  headers: {
-    'apikey': '53d3c02e056edba8db0c02bf0dea15ed',
-  },
-})
-  .then(response => response.json())
-  .then(data => {
-    console.log(data); 
-    const propertyList = document.getElementById('property-list');
-    data.property.forEach(property => {
-      const propertyElement = document.createElement('div');
-      propertyElement.className = 'property';
-      propertyElement.innerHTML = `
-        <div class="property-details">
-          <div>Address: ${property.address}</div>
-          <div>City: ${property.identifier}</div>
-          <div>State: ${property.location}</div>
-          <div>Zip: ${property.vintage}</div>
-        </div>
-      `;
-      propertyList.appendChild(propertyElement);
-    });
-  })
-  .catch(error => console.error('Error:'+ error));
+            productsContainer.innerHTML += productHTML;
+        });
+    })
+    
+    .catch(error => console.error('Error:'+ error));
 
-  
